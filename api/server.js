b4de881b344c23978e790181578d68c5d14d982f
BuschFranco <francobusch130@gmail.com>
2024-08-21 04:31:19 -0300

    testeando

--- api/server.js
+++ api/server.js
@@ -6,7 +6,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello from Express API!' });
+  res.json({ message: 'Hello from Express APIs!' });
 });
 
 export default (req, res) => {
